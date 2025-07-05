f36fb820b7fa10e1b3590f1e9ef8b5aa4c5764b5
seungchan <tmdcks3489@gmail.com>
2025-07-05 18:33:09 +0900

    🎉 Begin

---
+++ src/main/java/com/quickboard/resourceprofile/ResourceProfileApplication.java
@@ -0,0 +1,13 @@
+package com.quickboard.resourceprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickboard/resourceprofile/ResourceProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickboard.resourceprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
