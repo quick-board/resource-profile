45784fdb539638bb914e4f29220d7f2f745bebef
seungchan <tmdcks3489@gmail.com>
2025-07-05 18:37:27 +0900

    📝 Init

--- src/main/java/com/quickboard/resourceprofile/ResourceProfileApplication.java
+++ src/main/java/com/quickboard/resourceprofile/ResourceProfileApplication.java
@@ -2,8 +2,10 @@ package com.quickboard.resourceprofile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ResourceProfileApplication {
 
 	public static void main(String[] args) {
