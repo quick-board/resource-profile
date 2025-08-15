e74556515d1af6153adbb85c0f2c6319a0c326fa
seungchan <tmdcks3489@gmail.com>
2025-08-15 20:29:05 +0900

    🐛 Fix

--- src/main/java/com/quickboard/resourceprofile/ResourceProfileApplication.java
+++ src/main/java/com/quickboard/resourceprofile/ResourceProfileApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ResourceProfileApplication {
 
 	public static void main(String[] args) {
